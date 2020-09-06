b9e238d35a4464582fda773ce4a9848c4fe18c1a
Christian Van Loon <66512995+WarMachine-282@users.noreply.github.com>
2020-09-06 16:35:13 -0700

    update

--- index.js
+++ index.js
@@ -1,23 +1,25 @@
 const form = document.getElementById("monstersForm");
 
 function costOfMonster(num1, num2) {
-    return (num1 * num2);
+  return num1 * num2;
 }
 
-function totalCost(e){
-    e.preventDefault();
+function totalCost(e) {
+  e.preventDefault();
 
-    let numberOfGoombas = costOfMonster(form.goombas.value, 5);
-    let numberOfBobombs = costOfMonster(form.bobombs.value, 7);
-    let numberOfCheepcheeps = costOfMonster(form.cheepcheeps.value, 11);
+  let numberOfGoombas = costOfMonster(form.goombas.value, 5);
+  let numberOfBobombs = costOfMonster(form.bobombs.value, 7);
+  let numberOfCheepcheeps = costOfMonster(form.cheepcheeps.value, 11);
 
-    let result = (numberOfGoombas + numberOfBobombs + numberOfCheepcheeps);
+  let result = numberOfGoombas + numberOfBobombs + numberOfCheepcheeps;
 
-    document.getElementById("totalCost").innerHTML += `Your total cost is ${result} coins.`;
+  document.getElementById(
+    "totalCost"
+  ).innerHTML += `Your total cost is ${result} coins.`;
 
-    form.goombas.value = " ";
-    form.bobombs.value = " ";
-    form.cheepcheeps.value = " ";
+  form.goombas.value = " ";
+  form.bobombs.value = " ";
+  form.cheepcheeps.value = " ";
 }
 
 form.addEventListener("submit", totalCost);
